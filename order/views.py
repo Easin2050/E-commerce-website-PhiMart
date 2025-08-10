d396eea8bbb5bd8f4fca68788bf1bc7267242ef4
easin <easin562050@gmail.com>
2025-08-10 20:22:15 +0600

    product class based model done

--- order/views.py
+++ order/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework.viewsets import GenericViewSet
+from order.serializers import CartSeriallizer
+from rest_framework.mixins import CreateModelMixin
+from order.models import Cart
 # Create your views here.
+
+class CartViewSet(CreateModelMixin,GenericViewSet):
+    queryset=Cart.objects.all()
+    serializer_class=CartSeriallizer
--- product/product_urls.py
+++ product/product_urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from product import views
 
 urlpatterns = [
-    path('<int:id>',views.view_specific_product,name='product-list'),
-    path('',views.view_products,name='product-lists'),
+    path('<int:id>',views.ViewSpecificProduct.as_view(),name='product-list'),
+    path('',views.ViewProducts.as_view(),name='product-lists'),
 ]
