bffe2d6ddb4b84c7f6f3ab9f17c3ba00062b8086
easin <easin562050@gmail.com>
2025-08-12 08:39:14 +0600

    Product implemented by viewset

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path,include
+from rest_framework.routers import SimpleRouter
+from product.views import ProductViewSet
+
+router=SimpleRouter()
+router.register('products',ProductViewSet)
 
 urlpatterns = [
     path('products/',include('product.product_urls')),
--- product/views.py
+++ product/views.py
@@ -9,7 +9,19 @@ from django.db.models import Count
 from rest_framework.views import APIView
 from rest_framework.mixins import CreateModelMixin
 from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
-# Create your views here.
+from rest_framework.viewsets import ModelViewsSet
+
+class ProductViewSet(ModelViewsSet):
+    queryset=Product.objects.all()
+    serializer_class=ProductSerializer
+
+    def destroy(self,request,*args,**kwargs):
+        product=self.get_object()
+        if product.stock>10:
+            return Response({'message': 'Product with stock more than 10 can not be deleted'})
+        self.perform_destroy(product)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 # It is used for reduce redundent code and to use the same serializer for both list and create operations.
 class ProductList(ListCreateAPIView):
