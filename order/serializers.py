8300f4cdda1299bd9f27222f6e846b66ead3665e
easin <easin562050@gmail.com>
2025-08-13 12:26:03 +0600

    order and cart model done

--- order/serializers.py
+++ order/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
 from order.models import Cart,CartItem
 
+class CartItemSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model=CartItem
+        fields=['id','product','quantity']
+
+    def get_product_price(self,cart_item):
+        return cart_item.product.price
+
 class CartSeriallizer(serializers.ModelSerializer):
+    items=CartItemSerializer(many=True)
     class Meta:
         model=Cart
-        fields=['id','user']
+        fields=['id','user','items']
 
--- order/views.py
+++ order/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from rest_framework.viewsets import GenericViewSet
 from order.serializers import CartSeriallizer
-from rest_framework.mixins import CreateModelMixin
+from rest_framework.mixins import CreateModelMixin,RetrieveModelMixin
 from order.models import Cart
 # Create your views here.
 
-class CartViewSet(CreateModelMixin,GenericViewSet):
+class CartViewSet(CreateModelMixin,RetrieveModelMixin,GenericViewSet):
     queryset=Cart.objects.all()
     serializer_class=CartSeriallizer
