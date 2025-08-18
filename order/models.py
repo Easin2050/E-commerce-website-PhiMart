ab94cf30625a7f42b33f668564ca1e8827122224
easin <easin562050@gmail.com>
2025-08-18 11:01:50 +0600

    order done

--- order/models.py
+++ order/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 from users.models import User
 from product.models import Product
 from uuid import uuid4
@@ -6,7 +7,7 @@ from uuid import uuid4
 
 
 class Cart(models.Model):
-    id=models.UUIDField(primary_key=True, default=uuid4,editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, related_name="cart")
     created_at = models.DateTimeField(auto_now_add=True)
@@ -19,10 +20,10 @@ class CartItem(models.Model):
     cart = models.ForeignKey(
         Cart, on_delete=models.CASCADE, related_name="items")
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(validators=[MinValueValidator(1)])
 
     class Meta:
-        unique_together = [('cart', 'product')]
+        unique_together = [['cart', 'product']]
 
     def __str__(self):
         return f"{self.quantity} x {self.product.name}"
