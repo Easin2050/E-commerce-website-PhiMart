6f37c6297cee4a4815d4987db984f1c3ecf11080
easin <easin562050@gmail.com>
2025-08-20 10:39:50 +0600

    custom permission done

--- product/views.py
+++ product/views.py
@@ -15,7 +15,8 @@ from product.filters import ProductFilter
 from rest_framework.filters import SearchFilter,OrderingFilter
 from rest_framework.pagination import PageNumberPagination
 from product.paginations import DefaultPagination
-from rest_framework.permissions import IsAdminUser,AllowAny
+# from rest_framework.permissions import IsAdminUser,AllowAny
+from api.permissons import IsAdminOrReadOnly
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
@@ -28,11 +29,12 @@ class ProductViewSet(ModelViewSet):
     search_fields = ['name', 'description']
     ordering_fields = ['price','updated_at']
     # permission_classes=[IsAdminUser]
+    permission_classes=[IsAdminOrReadOnly]
 
-    def get_permissions(self):
+    '''def get_permissions(self):
         if self.request.method=='GET':
             return[AllowAny()]
-        return[IsAdminUser()]
+        return[IsAdminUser()]'''
 
     def destroy(self,request,*args,**kwargs):
         product=self.get_object()
@@ -43,6 +45,7 @@ class ProductViewSet(ModelViewSet):
 
 
 class CategoryViewSet(ModelViewSet):
+    permission_classes=[IsAdminOrReadOnly]
     queryset=Category.objects.annotate(product_count=Count('products')).all()
     serializer_class=CategorySerializer
 
