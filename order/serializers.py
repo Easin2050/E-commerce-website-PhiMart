b62b043e8766bee630b3ac4641b1f98a06706efe
easin <easin562050@gmail.com>
2025-08-24 21:36:54 +0600

    action for update status done

--- order/serializers.py
+++ order/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
-from order.models import Cart, CartItem
+from order.models import Cart, CartItem, Order, OrderItem
 from product.models import Product
 from product.serializers import ProductSerializer
-from order.models import Order,OrderItem
 from order.services import OrderService
+
+
+class EmptySerializer(serializers.Serializer):
+    pass
+
 
 class SimpleProductSerializer(serializers.ModelSerializer):
     class Meta:
@@ -62,7 +66,8 @@ class CartItemSerializer(serializers.ModelSerializer):
 
 class CartSerializer(serializers.ModelSerializer):
     items = CartItemSerializer(many=True, read_only=True)
-    total_price = serializers.SerializerMethodField()
+    total_price = serializers.SerializerMethodField(
+        method_name='get_total_price')
 
     class Meta:
         model = Cart
@@ -70,46 +75,50 @@ class CartSerializer(serializers.ModelSerializer):
         read_only_fields = ['user']
 
     def get_total_price(self, cart: Cart):
-        return sum([item.product.price * item.quantity for item in cart.items.all()])
-
+        return sum(
+            [item.product.price * item.quantity for item in cart.items.all()])
 
 
 class CreateOrderSerializer(serializers.Serializer):
-    cart_id=serializers.UUIDField()
+    cart_id = serializers.UUIDField()
 
     def validate_cart_id(self, cart_id):
         if not Cart.objects.filter(pk=cart_id).exists():
-            raise serializers.ValidationError('No Cart Found')
+            raise serializers.ValidationError('No cart found with this id')
+
         if not CartItem.objects.filter(cart_id=cart_id).exists():
             raise serializers.ValidationError('Cart is empty')
-        
+
         return cart_id
 
-    def create(self,validated_data):
-        user_id=self.context['user_id']
-        cart_id=validated_data['cart_id']
+    def create(self, validated_data):
+        user_id = self.context['user_id']
+        cart_id = validated_data['cart_id']
 
         try:
-            order=OrderService.create_order(user_id=user_id,cart_id=cart_id)
+            order = OrderService.create_order(user_id=user_id, cart_id=cart_id)
             return order
         except ValueError as e:
             raise serializers.ValidationError(str(e))
-    
+
     def to_representation(self, instance):
         return OrderSerializer(instance).data
 
+
 class OrderItemSerializer(serializers.ModelSerializer):
-    product=SimpleProductSerializer()
+    product = SimpleProductSerializer()
+
     class Meta:
-        model=OrderItem
-        fields=['id','product','quantity','price','total_price']
+        model = OrderItem
+        fields = ['id', 'product', 'price', 'quantity', 'total_price']
+
 
 class UpdateOrderSerializer(serializers.ModelSerializer):
     class Meta:
-        model=Order
-        fields=['status']
-
-    def update(self,instance,validated_data):
+        model = Order
+        fields = ['status']
+    
+    '''def update(self,instance,validated_data):
         user=self.context['user']
         new_status=validated_data['status']
 
@@ -119,11 +128,11 @@ class UpdateOrderSerializer(serializers.ModelSerializer):
         if not user.is_staff:
             raise serializers.ValidationError({'detail':'You are not allowed to update this order'})
         
-        return super.update(instance,validated_data)
+        return super.update(instance,validated_data)'''
 
 class OrderSerializer(serializers.ModelSerializer):
-    items=OrderItemSerializer(many=True)
-    class Meta:
-        model=Order
-        fields=['id','user','status','total_price','created_at','items']
+    items = OrderItemSerializer(many=True)
 
+    class Meta:
+        model = Order
+        fields = ['id', 'user', 'status', 'total_price', 'created_at', 'items']
